a309d3122990e3d1043761e3f032cd00c6f9e115
Bashar Talafha <bashartalafha@users.noreply.github.com>
2018-04-08 16:08:07 +0300

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -7,3 +7,22 @@ class Utility(object):
         line = re.sub(' +',' ', line)
         return line.strip()     
     
+    @staticmethod
+    def read_dataset(dataset):
+        path="dataset/VQAMed2018"+dataset+"/VQAMed2018"+dataset+"-QA.csv"
+        df =pd.read_csv(path, sep='\t', header=None, quoting=csv.QUOTE_NONE)
+        if "Test" in dataset:
+            df = df.rename(columns={0: 'id', 1: 'image_name', 2:"question"})
+        else:
+            df = df.rename(columns={0: 'id', 1: 'image_name', 2:"question", 3:"answer"})
+        print(dataset+" data size=",len(df))
+        images = []
+        for i in df["image_name"]:
+
+            fname = "dataset/VQAMed2018"+dataset+"/VQAMed2018"+dataset+"-images/"+i+".jpg"
+            images.append(fname)
+        if "Test" in dataset:
+            return images, df["question"]
+        else:
+            return images, df["question"], df["answer"]
+
